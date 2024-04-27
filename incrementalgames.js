a13b47449b70ace2ffd45ffb87e0ddbe4a60876a
loader3229 <1010903229@qq.com>
2024-04-28 02:33:26 +0800

    commit

---
+++ incrementalgames.js
@@ -0,0 +1,4 @@
+try{
+	$("#milestone1").html(JSON.parse(atob(localStorage.c2nv4in9eusojg59bmo)).m.points);
+	$("#milestone2").html(JSON.parse(atob(localStorage.c2nv4in9eusojg59bmo)).m.points);
+}catch(e){}
