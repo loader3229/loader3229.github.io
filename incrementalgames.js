eb945dadf4e116bbbbd28252f35c9fe69c057add
loader3229 <1010903229@qq.com>
2024-05-03 23:21:34 +0800

    commit

--- incrementalgames.js
+++ incrementalgames.js
@@ -2,3 +2,11 @@ try{
 	$("#milestone1").html(JSON.parse(atob(localStorage.c2nv4in9eusojg59bmo)).m.points);
 	$("#milestone2").html(JSON.parse(atob(localStorage.c2nv4in9eusojg59bmo)).m.points);
 }catch(e){}
+
+try{
+	var tmp=0;
+	for(var i=1;i<=7;i++){tmp+=parseInt(JSON.parse(atob(localStorage.multitree)).tm.buyables[i]);};
+	$("#multitree1").html(tmp);
+	$("#multitree2").html(tmp);
+}catch(e){}
+
