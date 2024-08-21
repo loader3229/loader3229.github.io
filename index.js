d38b06faea2e38ee811771214405519bf96158fa
loader3229 <1010903229@qq.com>
2024-08-21 21:01:39 +0800

    commit

--- index.js
+++ index.js
@@ -927,9 +927,9 @@ pldiv.id='pldiv';
 document.body.append(pldiv);
 
 setInterval(function(){
-	var pl=(Math.log10(parseFloat(localStorage.pageopencount)/10+1)+Math.min(Math.log10(parseFloat(localStorage.kasumiIntimacy)/1000+1),9)+Math.log10((total_points+player.metapoints.add(1).log10().mul(10).toNumber()+player.metaprestige.add(1).log10().mul(30).toNumber()+player.metatranscension.add(1).log10().mul(100).toNumber())/10+1)+(getTotalRating()-1777)/100)**1.5;
+	var pl=(Math.log10(parseFloat(localStorage.pageopencount)/10+1)+Math.min(Math.log10(parseFloat(localStorage.kasumiIntimacy)/1000+1),9)+Math.log10((total_points+player.metapoints.add(1).log10().mul(10).toNumber()+player.metaprestige.add(1).log10().mul(30).toNumber()+player.metatranscension.add(1).log10().mul(100).toNumber())/10+1)+(getTotalRating()-1777)/100+((sha512_256(localStorage.supporterCode+"loader3229").slice(2) == '97b4061c3a44e2950549613ba148eff34250441a9b3121698a15fcefdb4f5a')?1:0))**1.5;
 	pl=pl*2+1;
-	pldisp.innerHTML="Level "+Math.floor(pl)+", EXP:"+Math.floor((Math.log10(parseFloat(localStorage.pageopencount)/10+1)+Math.min(Math.log10(parseFloat(localStorage.kasumiIntimacy)/1000+1),9)+Math.log10((total_points+player.metapoints.add(1).log10().mul(10).toNumber()+player.metaprestige.add(1).log10().mul(30).toNumber()+player.metatranscension.add(1).log10().mul(100).toNumber())/10+1)+(getTotalRating()-1777)/100)*100)+"/"+Math.floor(((Math.floor(pl)/2)**(2/3))*100);
+	pldisp.innerHTML="Level "+Math.floor(pl)+", EXP:"+Math.floor((Math.log10(parseFloat(localStorage.pageopencount)/10+1)+Math.min(Math.log10(parseFloat(localStorage.kasumiIntimacy)/1000+1),9)+Math.log10((total_points+player.metapoints.add(1).log10().mul(10).toNumber()+player.metaprestige.add(1).log10().mul(30).toNumber()+player.metatranscension.add(1).log10().mul(100).toNumber())/10+1)+(getTotalRating()-1777)/100+((sha512_256(localStorage.supporterCode+"loader3229").slice(2) == '97b4061c3a44e2950549613ba148eff34250441a9b3121698a15fcefdb4f5a')?1:0))*100)+"/"+Math.floor(((Math.floor(pl)/2)**(2/3))*100);
 	plbar.style.width=((pl-Math.floor(pl))*100)+"%";
 	
 	if(programming_contest_mode){
