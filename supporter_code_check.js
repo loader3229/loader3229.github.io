acd7645a239055997b96dd2f7ed72c758265eb36
qq1010903229 <1010903229@qq.com>
2025-08-21 14:47:47 +0800

    commit

---
+++ supporter_code_check.js
@@ -0,0 +1,3 @@
+setInterval(function(){
+	window.supporterCodeInput = (sha512_256(localStorage.supporterCode+"loader3229").slice(2) == '7b083ed1d3579e19e6750ae10ce02d6bb178ee3bd60de5ffec1ae2572a68b8');
+},10);
