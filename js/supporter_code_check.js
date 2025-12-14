e93105319096ce8f501907a3b210eecf7da77487
loader3229 <1010903229@qq.com>
2025-12-14 23:50:03 +0806

    commit

---
+++ js/supporter_code_check.js
@@ -0,0 +1,3 @@
+setInterval(function(){
+	window.supporterCodeInput = (sha512_256(localStorage.supporterCode+"loader3229").slice(2) == '71fcb6c48d87276cfcaf7db32358649f23c82461d543509061a0e783f04be5');
+},10);
